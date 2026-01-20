021087d8be81a2af9fc3fcc212ed0e47242c9a02
Akshat Bhutra <akshatb@maqsoftware.com>
2026-01-21 00:14:18 +0530

    Assignment fixes

--- Techsara/src/evaluate.py
+++ Techsara/src/evaluate.py
@@ -4,7 +4,10 @@ def evaluate_model(model, X_test, y_test, return_f1=False):
     y_pred = model.predict(X_test)
     print("📊 Classification Report:\n", classification_report(y_test, y_pred))
     
-    f1 = f1_score(y_test, y_pred)
+    from sklearn.metrics import f1_score
+
+    f1 = f1_score(y_test, y_pred, zero_division=0)
+
     print(f"F1-score: {f1:.4f}")
 
     if return_f1:
